e8ff443fd80680e7ec76b5331db5c5640f4a377c
joaco369 <joako.2villarreal@gmail.com>
2024-09-19 16:21:05 -0300

    sorted code

--- StarWarsDemo/Repository.swift
+++ StarWarsDemo/Repository.swift
@@ -30,10 +30,4 @@ struct RepositoryTest: DataRepository {
   var url: URL {
     Bundle.main.url(forResource: "StarWars Test", withExtension: "json")!
   }
-
-func getData() throws -> [StarCard] {
-  let url = Bundle.main.url(forResource: "StarWars", withExtension: "json")!
-  let data = try Data(contentsOf: url)
-  let decoder = JSONDecoder()
-  decoder.keyDecodingStrategy = .convertFromSnakeCase
 }
